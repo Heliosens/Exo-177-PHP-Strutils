be63b4f63d1eaf4c3ab9bfec1aefc56bc0825af9
Sylvie-B <unsolo@hotmail.fr>
2022-02-02 11:29:04 +0100

    class, construct, method

---
+++ class/StrUtils.php
@@ -0,0 +1,53 @@
+<?php
+
+
+class StrUtils
+{
+    private string $str;
+
+    public function __construct ($str){
+        $this->setStr ($str);
+    }
+
+    /**
+     * @return string
+     */
+    public function getStr(): string
+    {
+        return $this->str;
+    }
+
+    /**
+     * @param string $str
+     */
+    public function setStr(string $str): void
+    {
+        $this->str = $str;
+    }
+
+    public function bold ($item) : string {
+        $this->setStr($item);
+        return "<strong>" . $this->getStr() . "</strong>";
+    }
+
+    public function italic ($item) : string {
+        $this->setStr($item);
+        return "<i>" . $this->getStr() . "</i>";
+    }
+
+    public function underline ($item) : string {
+        $this->setStr($item);
+        return "<u>" . $this->getStr() . "</u>";
+    }
+
+    public function capitalize ($item) : string {
+        $this->setStr($item);
+        return strtoupper($this->getStr());
+    }
+
+    public function uglify ($item) : string {
+        $this->setStr($item);
+        return $this->underline($this->italic($this->bold($this->getStr())));
+    }
+
+}
--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
 
 // TODO n'oubliez pas de créer votre classe dans le dossier classes.
+require './class/StrUtils.php';
+
+$newStr = new StrUtils('a word to test');
+
+echo "<p>" . $newStr->bold('nouveau') . "</p>";
+
+echo "<br><br>";
+
+echo "<p>" . $newStr->italic('essai') . "</p>";
+
+echo "<br><br>";
+
+echo "<p>" . $newStr->underline('suite') . "</p>";
+
+echo "<br><br>";
+
+echo "<p>" . $newStr->capitalize('de l\'exo') . "</p>";
+
+echo "<br><br>";
+
+echo "<p>" . $newStr->uglify('test final') . "</p>";
 
